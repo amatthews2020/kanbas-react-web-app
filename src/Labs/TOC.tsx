9babc8b218a97fd2ac1e02ce4f2fc6c10bba23a5
Andrew Matthews <andyrew3000@gmail.com>
2024-09-24 14:50:07 -0400

    On A1

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,7 +7,8 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
-        <li><a href="https://github.com/amatthews2020/kanbas-react-web-app" id="wd-github" target="_blank">Github</a></li>
+        <li><a href="https://github.com/amatthews2020/kanbas-react-web-app/tree/a1" id="wd-github" target="_blank">
+          Github A1 Repo</a></li>
       </ul>
     );
   }
