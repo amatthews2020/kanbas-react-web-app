cc4db8b8e4ee96f0416f0db882888d12c334af5a
Andrew Matthews <andyrew3000@gmail.com>
2024-10-22 13:22:38 -0400

    a3 JavaScript

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,161 +1,174 @@
+import { useParams } from "react-router";
+import * as db from "../../Database";
+import { Link } from "react-router-dom";
+
 export default function AssignmentEditor() {
+    const { cid, aid } = useParams();
+    const assignments = db.assignments;
     return (
-      <div id="wd-assignments-editor">
-
-            <div className="mb-3 row">
-                <label htmlFor="assignment1"
-                    className="col-sm-5 col-form-label">
-                    Assignment Name </label>
-                <div className="col-sm-8">
-                    <input type="text" className="form-control"
-                        id="assignment1" value="A1" />
-                </div> 
-            </div>
-            <div className="mb-3 row">
-                <div className="col-sm-8">
-                    <textarea className="form-control"
-                        id="textarea2" rows={3} cols={50}>
-                        The assignment is available online.
-                        Submit a link to your application running on Netifly
-                    </textarea>
-                </div>
-            </div>
-            <div className="mb-3 row py-2">
-                <label htmlFor="points"
-                    className="col-sm-1 col-form-label">
-                    Points </label>
-                <div className="col-sm-7">
-                    <input type="text" className="form-control"
-                        id="points" value="100" />
-                </div> 
-            </div>
-            <div className="row py-2">
-                <label htmlFor="wd-assignment-groups"
-                    className="col-sm-2 col-form-label">
-                    Assignment Groups </label>
-                <div className="col-sm-6">
-                    <select id="wd-assignment-groups" className="form-select col-sm-5"> 
-                        <option selected> ASSIGNMENTS</option>
-                    </select>
-                </div>
-            </div>
-            <div className="my-3 row py-2">
-                <label htmlFor="wd-grades"
-                    className="col-sm-2 col-form-label">
-                    Display Grade as </label>
-                <div className="col-sm-6">
-                    <select id="wd-grades" className="form-select col-sm-5"> 
-                        <option selected> Percentage</option>
-                        <option>Raw Score</option>
-                    </select>
-                </div>
-            </div>
-            <div>
-                <div className="row">
-                <label htmlFor="wd-sub-type"
-                    className="col-sm-2 col-form-label">
-                    Submission Type </label>
-
-                <div className="col-sm-6 border border-secondary rounded">
-                    <div className="col-sm-12 py-2">
-                        <select id="wd-sub" className="form-select col-sm-5"> 
-                            <option selected> Online</option>
-                        </select>
-                    </div>
-                    <label className="row col-form-label px-2">
-                        Online Submission Type
-                    </label>
-                    <div className="form-check my-1">
-                            <input className="form-check-input" type="checkbox" 
-                                    id="text-entry" />
-                            <label className="form-check-label" htmlFor="text-entry">
-                                Text Entry </label>     
-                    </div>
-                    <div className="form-check my-1">
-                            <input className="form-check-input" type="checkbox" 
-                                    id="website-url" />
-                            <label className="form-check-label" htmlFor="website-url">
-                                Website URL </label>     
-                    </div>
-                    <div className="form-check my-1">
-                            <input className="form-check-input" type="checkbox" 
-                                    id="media-recording" />
-                            <label className="form-check-label" htmlFor="media-recording">
-                                Media Recording </label>     
-                    </div>
-                    <div className="form-check my-1">
-                            <input className="form-check-input" type="checkbox" 
-                                    id="student-annotation" />
-                            <label className="form-check-label" htmlFor="student-annotation">
-                                Student Annotation </label>     
-                    </div>
-                    <div className="form-check my-1">
-                            <input className="form-check-input" type="checkbox" 
-                                    id="file-uploads" />
-                            <label className="form-check-label" htmlFor="file-uploads">
-                                File Uploads </label>     
-                    </div>
-                     
-                    </div> 
-                </div>
-            </div>
-
-            <div>
-                <div className="row my-3">
-                    <label htmlFor="wd-assign"
-                        className="col-sm-2 col-form-label">
-                        Assign </label>
-
-                    <div className="border border-secondary rounded col-sm-6">
-                        <div className="row py-2 px-2">
-                            <label className="col-form-label" htmlFor="assign-to">
-                            Assign To
-                            </label>
+        <div>
+            {assignments
+                .filter((assignment: any) => assignment._id === aid)
+                .map((assignment: any) => (
+                    <div id="wd-assignments-editor">
+                    <div className="mb-3 row">
+                        <label htmlFor="assignment1"
+                            className="col-sm-5 col-form-label">
+                            Assignment Name </label>
+                        <div className="col-sm-8">
                             <input type="text" className="form-control"
-                            id="assign-to" placeholder="Everybody" />
+                                id="assignment1" value={assignment.title} />
+                        </div> 
+                    </div>
+                    <div className="mb-3 row">
+                        <div className="col-sm-8">
+                            <textarea className="form-control"
+                                id="textarea2" rows={3} cols={50}>
+                                {assignment.desc}
+                            </textarea>
                         </div>
+                    </div>
+                    <div className="mb-3 row py-2">
+                        <label htmlFor="points"
+                            className="col-sm-1 col-form-label">
+                            Points </label>
+                        <div className="col-sm-7">
+                            <input type="text" className="form-control"
+                                id="points" value={assignment.points} />
+                        </div> 
+                    </div>
+                    <div className="row py-2">
+                        <label htmlFor="wd-assignment-groups"
+                            className="col-sm-2 col-form-label">
+                            Assignment Groups </label>
+                        <div className="col-sm-6">
+                            <select id="wd-assignment-groups" className="form-select col-sm-5"> 
+                                <option selected> ASSIGNMENTS</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="my-3 row py-2">
+                        <label htmlFor="wd-grades"
+                            className="col-sm-2 col-form-label">
+                            Display Grade as </label>
+                        <div className="col-sm-6">
+                            <select id="wd-grades" className="form-select col-sm-5"> 
+                                <option selected> Percentage</option>
+                                <option>Raw Score</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="row">
+                        <label htmlFor="wd-sub-type"
+                            className="col-sm-2 col-form-label">
+                            Submission Type </label>
 
-                        <div className="row px-2">
-                            <label className="row col-form-lable px-4" htmlFor="due-date">
-                                Due
+                        <div className="col-sm-6 border border-secondary rounded">
+                            <div className="col-sm-12 py-2">
+                                <select id="wd-sub" className="form-select col-sm-5"> 
+                                    <option selected> Online</option>
+                                </select>
+                            </div>
+                            <label className="row col-form-label px-2">
+                                Online Submission Type
                             </label>
-                            <input type="date" className="form-control"
-                                id="due-date" value="2024-05-13" />
-                        </div>
-
-                        <div className="row py-2">
-                            <div className="col-sm-6">
-                                <label className="row col-form-lable px-4" htmlFor="available-from">
-                                    Available From
-                                </label>
-                                <input type="date" className="form-control"
-                                id="available-from" value="2024-05-06" />
+                            <div className="form-check my-1">
+                                    <input className="form-check-input" type="checkbox" 
+                                            id="text-entry" />
+                                    <label className="form-check-label" htmlFor="text-entry">
+                                        Text Entry </label>     
                             </div>
-                            <div className="col-sm-6">
-                                <label className="row col-form-lable px-4" htmlFor="available-to">
-                                    To
-                                </label>
-                                <input type="date" className="form-control"
-                                id="available-to" value="2024-05-13" />
+                            <div className="form-check my-1">
+                                    <input className="form-check-input" type="checkbox" 
+                                            id="website-url" />
+                                    <label className="form-check-label" htmlFor="website-url">
+                                        Website URL </label>     
                             </div>
-                            <div className="col-sm-6"></div>
+                            <div className="form-check my-1">
+                                    <input className="form-check-input" type="checkbox" 
+                                            id="media-recording" />
+                                    <label className="form-check-label" htmlFor="media-recording">
+                                        Media Recording </label>     
+                            </div>
+                            <div className="form-check my-1">
+                                    <input className="form-check-input" type="checkbox" 
+                                            id="student-annotation" />
+                                    <label className="form-check-label" htmlFor="student-annotation">
+                                        Student Annotation </label>     
+                            </div>
+                            <div className="form-check my-1">
+                                    <input className="form-check-input" type="checkbox" 
+                                            id="file-uploads" />
+                                    <label className="form-check-label" htmlFor="file-uploads">
+                                        File Uploads </label>     
+                            </div>
+                            
+                            </div> 
                         </div>
                     </div>
 
-                </div>
-            </div>
+                    <div>
+                        <div className="row my-3">
+                            <label htmlFor="wd-assign"
+                                className="col-sm-2 col-form-label">
+                                Assign </label>
 
-            <div className="row mt-5">
-                <div className="col-8">
-                <hr />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-8">
-                    <button className="btn border border-dark btn-danger me-1 float-end">Save</button>
-                    <button className="btn border border-dark btn-secondary me-1 float-end">Cancel</button>
-                </div>
-            </div>
-    </div>
+                            <div className="border border-secondary rounded col-sm-6">
+                                <div className="row py-2 px-2">
+                                    <label className="col-form-label" htmlFor="assign-to">
+                                    Assign To
+                                    </label>
+                                    <input type="text" className="form-control"
+                                    id="assign-to" placeholder="Everybody" />
+                                </div>
+
+                                <div className="row px-2">
+                                    <label className="row col-form-lable px-4" htmlFor="due-date">
+                                        Due
+                                    </label>
+                                    <input type="date" className="form-control"
+                                        id="due-date" value={assignment.due} />
+                                </div>
+
+                                <div className="row py-2">
+                                    <div className="col-sm-6">
+                                        <label className="row col-form-lable px-4" htmlFor="available-from">
+                                            Available From
+                                        </label>
+                                        <input type="date" className="form-control"
+                                        id="available-from" value={assignment.available} />
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <label className="row col-form-lable px-4" htmlFor="available-to">
+                                            To
+                                        </label>
+                                        <input type="date" className="form-control"
+                                        id="available-to" value={assignment.due} />
+                                    </div>
+                                    <div className="col-sm-6"></div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="row mt-5">
+                        <div className="col-8">
+                        <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-8">
+                        <Link  id="wd-signin-btn"
+                            to={`/Kanbas/Courses/${cid}/Assignments`}
+                            className="btn border border-dark btn-danger me-1 float-end"> Save </Link>
+                        <Link  id="wd-signin-btn"
+                            to={`/Kanbas/Courses/${cid}/Assignments`}
+                            className="btn border border-dark btn-secondary me-1 float-end"> Cancel </Link>
+                        </div>
+                    </div>
+                    </div> 
+    ))} </div>
 );}
   
--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,152 +1,31 @@
 import { Link } from "react-router-dom";
+import * as db from "../Database";
+
 export default function Dashboard() {
+  const courses = db.courses;
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (8)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        {/* Course 1 */}
-        <div id="wd-dashboard-courses" className="row">
-          <div className="row row-cols-1 row-cols-md-5 g-4">
-            <div className="wd-dashboard-course col" style={{ width: "270px" }}>
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+          {courses.map((course) => (
+            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
               <div className="card rounded-3 overflow-hidden">
-                <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                      to="/Kanbas/Courses/1234/Home">
-                  <img src="/images/CS1234.png" width="100%" height={160}/>
+                <Link to={`/Kanbas/Courses/${course._id}/Home`}
+                      className="wd-dashboard-course-link text-decoration-none text-dark" >
+                  <img src={`/images/${course._id}.jpg`} width="100%" height={160} />
                   <div className="card-body">
                     <h5 className="wd-dashboard-course-title card-title">
-                      CS1234 React JS
-                    </h5>
-                    <p className="wd-dashboard-course-title card-text">
-                        Full Stack software developer
-                    </p>
+                      {course.name} </h5>
+                    <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
+                      {course.description} </p>
                     <button className="btn btn-primary"> Go </button>
                   </div>
                 </Link>
               </div>
             </div>
-            <div className="wd-dashboard-course col" style={{ width: "270px" }}>
-              <div className="card rounded-3 overflow-hidden">
-                <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                      to="/Kanbas/Courses/1234/Home">
-                  <img src="/images/CS1100.jpg" width="100%" height={160}/>
-                  <div className="card-body">
-                    <h5 className="wd-dashboard-course-title card-title">
-                      CS 1100 Computer...
-                    </h5>
-                    <p className="wd-dashboard-course-title card-text">
-                      Introduces students to the...
-                    </p>
-                    <button className="btn btn-primary"> Go </button>
-                  </div>
-                </Link>
-              </div>
-            </div>
-            <div className="wd-dashboard-course col" style={{ width: "270px" }}>
-              <div className="card rounded-3 overflow-hidden">
-                <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                      to="/Kanbas/Courses/1234/Home">
-                  <img src="/images/CS1200.jpg" width="100%" height={160}/>
-                  <div className="card-body">
-                    <h5 className="wd-dashboard-course-title card-title">
-                      CS 1200 First Year...
-                    </h5>
-                    <p className="wd-dashboard-course-title card-text">
-                      Seeks to support students...
-                    </p>
-                    <button className="btn btn-primary"> Go </button>
-                  </div>
-                </Link>
-              </div>
-            </div>
-            <div className="wd-dashboard-course col" style={{ width: "270px" }}>
-              <div className="card rounded-3 overflow-hidden">
-                <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                      to="/Kanbas/Courses/1234/Home">
-                  <img src="/images/CS1800.jpg" width="100%" height={160}/>
-                  <div className="card-body">
-                    <h5 className="wd-dashboard-course-title card-title">
-                      CS 1800. Discrete...
-                    </h5>
-                    <p className="wd-dashboard-course-title card-text">
-                      Introduces the mathematic...
-                    </p>
-                    <button className="btn btn-primary"> Go </button>
-                  </div>
-                </Link>
-              </div>
-            </div>
-            <div className="wd-dashboard-course col" style={{ width: "270px" }}>
-              <div className="card rounded-3 overflow-hidden">
-                <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                      to="/Kanbas/Courses/1234/Home">
-                  <img src="/images/CS2500.jpg" width="100%" height={160}/>
-                  <div className="card-body">
-                    <h5 className="wd-dashboard-course-title card-title">
-                      CS 2500 Fundament...
-                    </h5>
-                    <p className="wd-dashboard-course-title card-text">
-                      Introduces the fundamenta...
-                    </p>
-                    <button className="btn btn-primary"> Go </button>
-                  </div>
-                </Link>
-              </div>
-            </div>
-            <div className="wd-dashboard-course col" style={{ width: "270px" }}>
-              <div className="card rounded-3 overflow-hidden">
-                <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                      to="/Kanbas/Courses/1234/Home">
-                  <img src="/images/CS2800.jpg" width="100%" height={160}/>
-                  <div className="card-body">
-                    <h5 className="wd-dashboard-course-title card-title">
-                      CS 2800 Logic and...
-                    </h5>
-                    <p className="wd-dashboard-course-title card-text">
-                      Introduces formal logic a...
-                    </p>
-                    <button className="btn btn-primary"> Go </button>
-                  </div>
-                </Link>
-              </div>
-            </div>
-            <div className="wd-dashboard-course col" style={{ width: "270px" }}>
-              <div className="card rounded-3 overflow-hidden">
-                <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                      to="/Kanbas/Courses/1234/Home">
-                  <img src="/images/CS3000.jpeg" width="100%" height={160}/>
-                  <div className="card-body">
-                    <h5 className="wd-dashboard-course-title card-title">
-                      CS 3000 Algorithms...
-                    </h5>
-                    <p className="wd-dashboard-course-title card-text">
-                      Introduces the basic princi...
-                    </p>
-                    <button className="btn btn-primary"> Go </button>
-                  </div>
-                </Link>
-              </div>
-            </div>
-            <div className="wd-dashboard-course col" style={{ width: "270px" }}>
-              <div className="card rounded-3 overflow-hidden">
-                <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                      to="/Kanbas/Courses/1234/Home">
-                  <img src="/images/CS4400.png" width="100%" height={160}/>
-                  <div className="card-body">
-                    <h5 className="wd-dashboard-course-title card-title">
-                      CS 4400 Programmin...
-                    </h5>
-                    <p className="wd-dashboard-course-title card-text">
-                      Introduces a systematic ap...
-                    </p>
-                    <button className="btn btn-primary"> Go </button>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
-    </div>
-  );
-}
+    </div>);}
